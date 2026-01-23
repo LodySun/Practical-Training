5e858b36cd19e696b171ed561243d19c53366e92
Lody <jianlun.sun@helsinki.fi>
2026-01-23 13:16:47 +0200

    Update table_gen.R

--- table_gen.R
+++ table_gen.R
@@ -1,95 +1,88 @@
 set.seed(42)
 
-# reference cards (slot -> feature)
+# ---- reference cards: slot -> feature ----
 slot_shape <- c("circle","triangle","cross","star")
 slot_color <- c("red","green","blue","yellow")
 slot_num   <- c("1","2","3","4")
 slot_dir   <- c("up","right","down","left")
 
-# mappings
+# ---- feature -> slot (consistent with reference cards) ----
 shape2slot <- c(circle=1, triangle=2, cross=3, star=4)
 color2slot <- c(red=1, green=2, blue=3, yellow=4)
 num2slot   <- c("1"=1, "2"=2, "3"=3, "4"=4)
 dir2slot   <- c(up=1, right=2, down=3, left=4)
 
-# stimulus pool
-shapes  <- c("triangle","cross","circle","star")
-colors  <- c("red","green","yellow","blue")
-numbers <- c("1","2","3","4")
-dirs    <- c("up","right","down","left")
-
-stim <- expand.grid(shape=shapes, number=numbers, color=colors, dir=dirs,
-                    stringsAsFactors = FALSE)
+# ---- stimulus pool ----
+stim <- expand.grid(
+  shape=c("triangle","cross","circle","star"),
+  number=c("1","2","3","4"),
+  color=c("red","green","yellow","blue"),
+  dir=c("up","right","down","left"),
+  stringsAsFactors=FALSE
+)
 stim$card <- paste0(stim$shape, stim$number, stim$color, "_", stim$dir)
 
-# helpers
+# ---- helpers (minimal set) ----
 parse_card <- function(card) {
-  m <- regexec("^([a-z]+)([1-4])([a-z]+)_([a-z]+)$", card)
-  g <- regmatches(card, m)[[1]]
-  if (length(g) != 5) return(NULL)
+  g <- regmatches(card, regexec("^([a-z]+)([1-4])([a-z]+)_([a-z]+)$", card))[[1]]
+  if (length(g) != 5) stop("Bad card format: ", card)
   list(shape=g[2], number=g[3], color=g[4], dir=g[5])
 }
 
-match_count_with_slot <- function(card_feats, j) {
-  sum(c(
-    card_feats$shape  == slot_shape[j],
-    card_feats$color  == slot_color[j],
-    card_feats$number == slot_num[j],
-    card_feats$dir    == slot_dir[j]
-  ))
+match_count <- function(cf, j) {
+  sum(cf$shape==slot_shape[j], cf$color==slot_color[j],
+      cf$number==slot_num[j], cf$dir==slot_dir[j])
 }
 
-correct_slot_for_rule <- function(card_feats, rule) {
-  if (rule == "shape")     return(shape2slot[card_feats$shape])
-  if (rule == "color")     return(color2slot[card_feats$color])
-  if (rule == "number")    return(num2slot[card_feats$number])
-  if (rule == "direction") return(dir2slot[card_feats$dir])
-  stop("unknown rule")
+slot_for_rule <- function(cf, rule) {
+  switch(rule,
+         shape=unname(shape2slot[cf$shape]),
+         color=unname(color2slot[cf$color]),
+         number=unname(num2slot[cf$number]),
+         direction=unname(dir2slot[cf$dir]),
+         stop("Unknown rule: ", rule))
 }
 
-is_unique_match <- function(card_feats, rule, strict_other_slots=TRUE) {
-  k <- correct_slot_for_rule(card_feats, rule)
-  if (match_count_with_slot(card_feats, k) != 1) return(FALSE)
-  if (strict_other_slots) {
-    for (j in setdiff(1:4, k)) {
-      if (match_count_with_slot(card_feats, j) >= 2) return(FALSE)
-    }
-  }
+unique_ok <- function(cf, rule) {
+  k <- slot_for_rule(cf, rule)
+  if (match_count(cf, k) != 1) return(FALSE)
+  for (j in setdiff(1:4, k)) if (match_count(cf, j) >= 2) return(FALSE)
   TRUE
 }
 
-# generate table
+q <- function(x) ifelse(grepl('^".*"$', x), x, paste0('"', x, '"'))
+
+# ---- generate then "fix" columns inline ----
 blocks <- 10
 trials_per_rule <- 10
 rules <- c("color","shape","number","direction")
 
-rows <- list()
-prev_rule <- NA
+out_rows <- vector("list", blocks * length(rules) * trials_per_rule)
+rowi <- 0
+prev_slot2 <- 0
 
 for (b in 1:blocks) {
   for (r in sample(rules)) {
-    i_in_block <- 0
-    while (i_in_block < trials_per_rule) {
-      idx <- sample(nrow(stim), 1)
-      cf  <- as.list(stim[idx, c("shape","number","color","dir")])
-      if (!is_unique_match(cf, r, TRUE)) next
-      i_in_block <- i_in_block + 1
+    for (seqn in 1:trials_per_rule) {
+      repeat {
+        idx <- sample(nrow(stim), 1)
+        cf  <- as.list(stim[idx, c("shape","number","color","dir")])
+        if (unique_ok(cf, r)) break
+      }
       card  <- stim$card[idx]
-      k     <- correct_slot_for_rule(cf, r)
-      prev  <- if (is.na(prev_rule)) 0 else correct_slot_for_rule(cf, prev_rule)
-      seqn  <- i_in_block
-      ruleq <- paste0('"', r, '"')
-      descq <- paste0('"', card, '"')
-      rows[[length(rows)+1]] <- c(card, k, prev, seqn, ruleq, descq)
+      slot2 <- slot_for_rule(cf, r)
+      slot3 <- if (seqn == 1) 0 else prev_slot2
+      prev_slot2 <- slot2
+
+      rowi <- rowi + 1
+      out_rows[[rowi]] <- c(card, slot2, slot3, seqn, q(r), q(card))
     }
-    prev_rule <- r
   }
 }
 
-wcst <- do.call(rbind, rows)
+out <- do.call(rbind, out_rows[1:rowi])
 
-write.table(wcst, file="wcst4d.txt",
-            sep=" ", quote=FALSE, row.names=FALSE, col.names=FALSE)
-
-cat(sprintf("Done! wcst4d.txt created (%d rows)\n", nrow(wcst)))
+outfile <- "wcst4d.txt"
+write.table(out, file=outfile, sep=" ", quote=FALSE, row.names=FALSE, col.names=FALSE)
+cat(sprintf("✅ Done! %s created (%d rows)\n", outfile, nrow(out)))
 
