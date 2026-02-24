7e89af4b695772dbaaee539710fdd1f7c7a80462
Lody <jianlun.sun@helsinki.fi>
2026-02-24 15:05:15 +0200

    Update table_gen.R

--- table_gen.R
+++ table_gen.R
@@ -7,17 +7,17 @@ slot_num   <- c("1","2","3","4")
 slot_dir   <- c("up","right","down","left")
 
 # ---- feature -> slot (consistent with reference cards) ----
-shape2slot <- c(circle=1, triangle=2, cross=3, star=4)
-color2slot <- c(red=1, green=2, blue=3, yellow=4)
-num2slot   <- c("1"=1, "2"=2, "3"=3, "4"=4)
-dir2slot   <- c(up=1, right=2, down=3, left=4)
+shape2slot <- c(circle = 1, triangle = 2, cross = 3, star = 4)
+color2slot <- c(red = 1, green = 2, blue = 3, yellow = 4)
+num2slot   <- c("1" = 1, "2" = 2, "3" = 3, "4" = 4)
+dir2slot   <- c(up = 1, right = 2, down = 3, left = 4)
 
 # ---- stimulus pool ----
 stim <- expand.grid(
-  shape=c("triangle","cross","circle","star"),
-  number=c("1","2","3","4"),
-  color=c("red","green","yellow","blue"),
-  dir=c("up","right","down","left"),
+  shape = c("triangle","cross","circle","star"),
+  number = c("1","2","3","4"),
+  color = c("red","green","yellow","blue"),
+  dir = c("up","right","down","left"),
   stringsAsFactors=FALSE
 )
 stim$card <- paste0(stim$shape, stim$number, stim$color, "_", stim$dir)
@@ -30,16 +30,16 @@ parse_card <- function(card) {
 }
 
 match_count <- function(cf, j) {
-  sum(cf$shape==slot_shape[j], cf$color==slot_color[j],
-      cf$number==slot_num[j], cf$dir==slot_dir[j])
+  sum(cf$shape == slot_shape[j], cf$color == slot_color[j],
+      cf$number == slot_num[j], cf$dir == slot_dir[j])
 }
 
 slot_for_rule <- function(cf, rule) {
   switch(rule,
-         shape=unname(shape2slot[cf$shape]),
-         color=unname(color2slot[cf$color]),
-         number=unname(num2slot[cf$number]),
-         direction=unname(dir2slot[cf$dir]),
+         shape = unname(shape2slot[cf$shape]),
+         color = unname(color2slot[cf$color]),
+         number = unname(num2slot[cf$number]),
+         direction = unname(dir2slot[cf$dir]),
          stop("Unknown rule: ", rule))
 }
 
@@ -60,9 +60,20 @@ rules <- c("color","shape","number","direction")
 out_rows <- vector("list", blocks * length(rules) * trials_per_rule)
 rowi <- 0
 prev_slot2 <- 0
+prev_block_last_rule <- NA_character_
 
 for (b in 1:blocks) {
-  for (r in sample(rules)) {
+  rule_order <- sample(rules)
+  # Enforce a rule switch at block boundary:
+  # first rule of current block must differ from last rule of previous block.
+  if (!is.na(prev_block_last_rule) && rule_order[1] == prev_block_last_rule) {
+    swap_idx <- which(rule_order != prev_block_last_rule)[1]
+    tmp <- rule_order[1]
+    rule_order[1] <- rule_order[swap_idx]
+    rule_order[swap_idx] <- tmp
+  }
+
+  for (r in rule_order) {
     for (seqn in 1:trials_per_rule) {
       repeat {
         idx <- sample(nrow(stim), 1)
@@ -73,16 +84,17 @@ for (b in 1:blocks) {
       slot2 <- slot_for_rule(cf, r)
       slot3 <- if (seqn == 1) 0 else prev_slot2
       prev_slot2 <- slot2
-
+      
       rowi <- rowi + 1
       out_rows[[rowi]] <- c(card, slot2, slot3, seqn, q(r), q(card))
     }
   }
+  prev_block_last_rule <- rule_order[length(rule_order)]
 }
 
 out <- do.call(rbind, out_rows[1:rowi])
 
 outfile <- "wcst4d.txt"
-write.table(out, file=outfile, sep=" ", quote=FALSE, row.names=FALSE, col.names=FALSE)
-cat(sprintf("✅ Done! %s created (%d rows)\n", outfile, nrow(out)))
+write.table(out, file = outfile, sep = " ", quote = FALSE, row.names = FALSE, col.names = FALSE)
+cat(sprintf("Done! %s created (%d rows)\n", outfile, nrow(out)))
 
